9755a68b4871e0385e22d2ae3c3915498862775e
SDTP Lem <134363399+CodeX-Lem@users.noreply.github.com>
2023-07-18 14:12:33 +0800

    First Commit 7/18/2023

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Product;
+use Illuminate\Http\Request;
+class ProductController extends Controller
+{
+  /**
+  * Display a listing of the resource.
+  */
+  public function index(Request $request)
+  {
+    $search = $request->input('search');
+    $products = Product::where('name', 'like', "%$search%")->get();
+
+    return response()->json($products);
+  }
+  /**
+  * Store a newly created resource in storage.
+  */
+  public function store(Request $request)
+  {
+    $product = Product::create($request->all());
+    return response()->json($product, 201);
+  }
+  /**
+  * Display the specified resource.
+  */
+  public function show(Product $product)
+  {
+    return response()->json($product);
+  }
+  /**
+  * Update the specified resource in storage.
+  */
+  public function update(Request $request, Product $product)
+  {
+    $product->update($request->all());
+    return response()->json($product);
+  }
+  /**
+  * Remove the specified resource from storage.
+  */
+  public function destroy(Product $product)
+  {
+    $product->delete();
+    return response()->json(null, 204);
+  }
+}
+
+
